58c394fcd97eef205524b290eebc56c178f42f9e
Shamao <994516742@qq.com>
2021-04-14 01:38:04 +0800

    临时提交

--- src/help.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "当前支持的工功能有:"
-echo "kae upload           用户模块上传, 仅支持settings.gradle 带:的模块"
-echo "kae grep [module]    查看appshell匹配module的内容"
-echo "kae adb  [fubc]      支持部分adb操作:top, layout-on, layout-off, over-on , over-off"
